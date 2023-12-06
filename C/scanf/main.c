50deedd164436025ccfe8a7dd7caf479cb32f1f9
Thalys001 <thalyssouza708@gmail.com>
2023-12-05 23:39:54 -0300

    feat: validation in age

--- C/scanf/main.c
+++ C/scanf/main.c
@@ -1,12 +1,44 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
 int main(void)
 {
-  char nome[20];
+  char nome[50];
+  char idadeStr[4];
   int idade;
+  int i, isNumber;
+
   printf("Digite o seu nome: ");
-  scanf("%s", nome);
-  printf("Digite a sua idade: ");
-  scanf("%d", &idade);
+  fgets(nome, sizeof(nome), stdin);
+  nome[strcspn(nome, "\n")] = 0;
+
+  while (1)
+  {
+    printf("Digite a sua idade: ");
+    fgets(idadeStr, sizeof(idadeStr), stdin);
+    idadeStr[strcspn(idadeStr, "\n")] = 0;
+
+    isNumber = 1;
+    for(i = 0; i < strlen(idadeStr); i++)
+    {
+      if (!isdigit(idadeStr[i]))
+      {
+        isNumber = 0;
+        break;
+      }
+    }
+
+    if (isNumber)
+    {
+      sscanf(idadeStr, "%d", &idade);
+      break;
+    }
+    else
+    {
+      printf("Por favor, digite apenas números para a idade.\n");
+    }
+  }
+
   printf("Nome: %s\nIdade: %d\n", nome, idade);
 }
