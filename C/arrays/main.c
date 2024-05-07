77d35bad9eab45b11a30d1895f2b156026f15ba9
Thalys001 <thalyssouza708@gmail.com>
2024-05-06 23:51:51 -0300

    feat: arrays examples

---
+++ C/arrays/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+// primeira definição
+// int prices[5]; 
+
+// usando uma constante para definição do tamamnho do array
+// const int SIZE = 5;
+// int prices[5]; 
+
+
+// inicializando um array no momento da definição
+// int prices[5] = {1, 2, 3, 4, 5 };
+
+// atribuindo valor após definição
+// int prices[5];
+
+// prices[0] = 1;
+// prices[1] = 2;
+// prices[2] = 3;
+// prices[3] = 4;
+// prices[4] = 5;
+
+// usando um laço para atribuir os valores
+// int prices[5];
+
+// for (int i = 0; i < 5; i++) {
+//   prices[i] = i + 1;
+// }
+
+// determinando o valor dos indices
+// prices[0]; valor do item do array: 1 
+// prices[1]; valor do item do array: 2 
